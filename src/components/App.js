374eeb9c50346bbb75df1e4886ba1fe4075d02e2
Caleb Breaux <cbrocomp@gmail.com>
2019-01-15 01:47:11 -0800

    Main app uses Material-UI

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,49 @@ import React from 'react';
 import { Route, Link, Redirect, Switch } from 'react-router-dom';
 import api from '../api';
 
-import { Container, Icon, Menu } from 'semantic-ui-react';
-
-import PlaceholderContent from './PlaceholderContent';
 import EmployeeList from './EmployeeList';
 import EmployeeOverview from './EmployeeOverview';
 import Login from './Login';
 import Signup from './Signup';
 
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Icon from '@material-ui/core/Icon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
 class App extends React.Component {
+    state = {
+        drawerOpen: false
+    }
+    toggleDrawer = () => {
+        this.setState({ drawerOpen: !this.state.drawerOpen })
+    }
+    logout = () => {
+        var config = { headers: { 'x-auth': sessionStorage.token } };
+        api.delete('/users/logout', config)
+            .then(res => {
+                console.log('logged out');
+                sessionStorage.removeItem('token');
+                this.forceUpdate();
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
     render() {
+        const { classes } = this.props
         if (!sessionStorage.token) {
             if (
                 window.location.pathname !== '/login' &&
@@ -26,54 +59,119 @@ class App extends React.Component {
                 </Switch>
             );
         }
-        return (
+        const drawer = (
             <div>
-                <Menu inverted={true}>
-                    <Menu.Item as={Link} to="/">
-                        <Icon name="home" />
-                        Dashboard
-                    </Menu.Item>
-                    <Menu.Item as={Link} to="/employees">
-                        <Icon name="users" />
-                        Employees
-                    </Menu.Item>
+                <div className={classes.toolbar} />
+                <Divider />
+                <List>
+                    <ListItem button component={Link} to="/employees" key="employees">
+                        <ListItemIcon><Icon>people</Icon></ListItemIcon>
+                        <ListItemText primary="Employees" />
+                    </ListItem>
+                </List>
+            </div>
 
-                    <Menu.Item as={Link} to="/placeholder">
-                        <Icon name="calendar" />
-                        test
-                    </Menu.Item>
+        )
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton
+                            className={classes.menuButton}
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.toggleDrawer}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap className={classes.grow}>
+                            SimpleTime
+                        </Typography>
+                        <Button className={classes.logoutButton} color="inherit" onClick={this.logout}>Logout</Button>
+                    </Toolbar>
+                </AppBar>
+                <nav className={classes.drawer}>
+                    <Hidden smUp implementation="css">
+                        <Drawer
+                            classes={{
+                                paper: classes.drawerPaper,
+                            }}
+                            variant="temporary"
+                            onClose={this.toggleDrawer}
+                            open={this.state.drawerOpen}
+                        >
+                            {drawer}
 
-                    <Menu.Item as="a" onClick={this.logout}>
-                        <Icon name="calendar" />
-                        logout
-                    </Menu.Item>
-                </Menu>
+                        </Drawer>
+                    </Hidden>
+                    <Hidden xsDown implementation="css">
+                        <Drawer
+                            classes={{
+                                paper: classes.drawerPaper,
+                            }}
+                            variant="permanent"
+                            open
+                        >
+                            {drawer}
 
-                <Container style={{ height: '100vh' }}>
+                        </Drawer>
+                    </Hidden>
+                </nav>
+
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
                     <Route path="/employees" component={EmployeeList} />
                     <Route
                         path="/employee_overview/:id"
                         component={EmployeeOverview}
                     />
-                    <Route path="/placeholder" component={PlaceholderContent} />
-                    <Route path="/" />
-                </Container>
+                    {/* <Route path="/placeholder" component={PlaceholderContent} /> */}
+                    <Route path="/" >
+                    </Route>
+
+                </main>
             </div>
         );
     }
-
-    logout = () => {
-        var config = { headers: { 'x-auth': sessionStorage.token } };
-        api.delete('/users/logout', config)
-            .then(res => {
-                console.log('logged out');
-                sessionStorage.removeItem('token');
-                this.forceUpdate();
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    };
 }
+const drawerWidth = 240;
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    appBar: {
+        marginLeft: drawerWidth,
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+    },
+    menuButton: {
+        marginRight: 20,
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+    },
+    logoutButton: {
+        marginLeft: -12
+    }
+});
 
-export default App;
+export default withStyles(styles)(App);

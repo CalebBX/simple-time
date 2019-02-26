75d8c26c4201be6dd1fe30bc891bb4878183403c
Caleb Breaux <cbrocomp@gmail.com>
2019-02-25 18:00:57 -0800

    Removed unused imports

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -3,27 +3,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import api from '../api';
 
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Icon from '@material-ui/core/Icon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 
 class EmployeeList extends React.Component {
@@ -41,7 +25,7 @@ class EmployeeList extends React.Component {
                 <div>
                     {this.state.users.map(user => (
                         <Card square className={classes.card}>
-                            <CardActionArea className={classes.cardActionArea} component={Link} to={`/employee_overview/${user._id}`}>
+                            <CardActionArea className={classes.cardActionArea} component={Link} to={`/employee-overview/${user._id}`}>
                                 <Grid container spacing={32}>
                                     <Grid item md={1} xs={12}>
                                         <Avatar alt="Remy Sharp" src="/assets/img_avatar.png" className={classes.avatar} />
@@ -58,14 +42,6 @@ class EmployeeList extends React.Component {
                     ))}
                 </div>
             </div>
-
-            // <List.Item
-            //     as={Link}
-            //     to={`/employee_overview/${user._id}`}
-            //     key={user._id}
-            // >
-
-
         );
     }
 
